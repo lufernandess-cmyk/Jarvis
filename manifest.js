f7ef0c0b0c1f4f1eeadaa13517eca475b2994ca5
lufernandess-cmyk <lufernandess@gmail.com>
2026-08-11 18:01:17 -0300

    manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "J.A.R.V.I.S",
+  "short_name": "Jarvis",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0B0E14",
+  "theme_color": "#00F0FF",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
